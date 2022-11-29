a9e7bfbf91ff45de52f5ace1a73caa2582b5d146
Halim <103418307+wheresmyweekend@users.noreply.github.com>
2022-11-29 10:48:24 +0800

    added new script

---
+++ open-tabs-ff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author : Halim
+# Title: Multi-tab Opener
+# Opens a bunch of tabs in firefox private window...
+# Use script by invoking ./open-tabs-ff.sh
+# Script starts here:
+
+# List of websites to open
+URLS=""https://wiki.mozilla.org/Firefox/CommandLineOptions" "https://github.com/wheresmyweekend/""
+
+# Invoke ff command
+for URL in ${URLS}; do
+    firefox -private-window ${URL}
+done
